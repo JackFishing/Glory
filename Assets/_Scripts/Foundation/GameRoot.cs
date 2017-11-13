0313ecc730f535882b9c5cef97b50e55493dfe35
Geng Xiangfei <xiangfei.geng@sidangte.net>
2017-11-13 13:22:07 +0800

    上传Glory

---
+++ Assets/_Scripts/Foundation/GameRoot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/*
+ *	
+ *  
+ *
+ *	by Xuanyi
+ *
+ */
+
+namespace MoleMole
+{
+	public class GameRoot : MonoBehaviour {
+
+        public void Start()
+        {
+            Singleton<UIManager>.Create();
+            Singleton<ContextManager>.Create();
+            Singleton<Localization>.Create();
+        }
+
+	}
+}
